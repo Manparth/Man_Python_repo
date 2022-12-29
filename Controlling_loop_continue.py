ff290075c8b9c1b9984e5f8d16d22f322658f49c
Manparth <48568528+Manparth@users.noreply.github.com>
2022-12-29 10:44:28 -0600

    Create Controlling_loop_continue.py

---
+++ Controlling_loop_continue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env puthon3.7
+
+#Python code beings here using continue statement 
+
+count =0 
+while count <10 :
+  
+#For printing only the odd numbers 
+  if count % 2 ==0 :
+    count +=1
+    continue 
+  print(f" We're counting odd numbers : {count}")
+  count +=1
+    
+  

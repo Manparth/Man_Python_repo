23571f24ddeedc481e97e517e8f7ffa33055194b
Manparth <48568528+Manparth@users.noreply.github.com>
2022-12-29 10:50:27 -0600

    Create Controlling_loop_break.py

---
+++ Controlling_loop_break.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/  python3.7
+
+#Python code starts here 
+
+count =1 
+while count < 10 :
+    if count %2 ==0 :
+        break
+    print(f"the odd number is :  ", count)
+    count += 1
+    
+  # Break stops the iteration of the loop entirely 
